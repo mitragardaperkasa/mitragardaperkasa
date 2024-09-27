4be6c34a385f1e458c5acfd2748b1403993d5ffa
mitragardaperkasa <wahyuekosaputro2000@gmail.com>
2024-09-27 19:53:40 +0700

    Add files via upload

---
+++ footerb4.php
@@ -0,0 +1,28 @@
+<style>
+	footer p{
+		padding-top: 30px;
+		padding-bottom: 40px;
+		margin-top: 30px;
+	}
+	footer{
+		bottom: 0px;
+		margin-bottom: -20px;
+
+	}
+
+	footer p a{
+		text-decoration: none;
+		color: white;
+		
+	}
+</style>
+
+
+<footer>
+	<div class="bg-primary">
+
+           <div class="col-sm-12 text-center">
+              <p>&copy; copyright 2024 | Mitra Garda Perkasa by. <a href="#">Wahyu Eko Shaputro </p>
+            </div>
+    </div>
+</footer>
